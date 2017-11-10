a0f383d62ddf3205e31b9fbf5dccf08a807bc1d3
符坤 <fukun@fukundeMacBook-Pro.local>
2017-11-10 15:27:09 +0800

    add table order,test for newOrder

--- dao/order.go
+++ dao/order.go
@@ -18,7 +18,24 @@
 
 package dao
 
+// order amountS 上限1e30
+
 type Order struct {
-	ID        int    `gorm:"column:id;primary_key;"`
-	OrderHash []byte `gorm:"column:order_hash;type:varchar(64);unique_index"`
+	ID                    int     `gorm:"column:id;primary_key;"`
+	Owner                 string  `gorm:"column:owner;type:varchar(42)"`
+	OrderHash             string  `gorm:"column:order_hash;type:varchar(82);unique_index"`
+	TokenS                string  `gorm:"column:token_s;type:varchar(42)"`
+	TokenB                string  `gorm:"column:token_b;type:varchar(42)"`
+	AmountS               []byte  `gorm:"column:amount_s;type:varchar(30)"`
+	AmountB               []byte  `gorm:"column:amount_b;type:varchar(30)"`
+	CreateTime            int     `gorm:"column:create_time"`
+	Ttl                   int     `gorm:"column:ttl"`
+	Salt                  int     `gorm:"column:salt"`
+	LrcFee                []byte  `gorm:"column:lrc_fee;type:varchar(128)"`
+	BuyNoMoreThanAmountB  bool    `gorm:"column:buy_nomore_than_amountb;type:bit"`
+	MarginSplitPercentage uint8   `gorm:"column:margin_split_percentage;type:tinyint(4)"`
+	V                     uint8   `gorm:"column:v;type:tinyint(4)"`
+	R                     string  `gorm:"column:r;type:varchar(62)"`
+	S                     string  `gorm:"column:s;type:varchar(62)"`
+	Price                 float64 `gorm:"column:price;type:decimal(16,8);"`
 }
