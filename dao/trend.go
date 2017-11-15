0a23de867bc518947a706e7a0e613f3ada5a1235
mmmmmagina <jaice_229@163.com>
2017-11-15 14:20:05 +0800

    init trendmanager & add go-cache for trend

--- dao/trend.go
+++ dao/trend.go
@@ -31,11 +31,13 @@ type Trend struct {
 	Market	              string  `gorm:"column:market;type:varchar(42)"`
 	AmountS               []byte  `gorm:"column:amount_s;type:varchar(30)"`
 	AmountB               []byte  `gorm:"column:amount_b;type:varchar(30)"`
-	CreateTime            int64   `gorm:"column:create_time"`
+	CreateTime            int64   `gorm:"column:create_time";type:bigint`
 	Open                  float64 `gorm:"column:open;type:decimal(28,16);"`
 	Close                 float64 `gorm:"column:close;type:decimal(28,16);"`
 	High                  float64 `gorm:"column:high;type:decimal(28,16);"`
 	Low                   float64 `gorm:"column:low;type:decimal(28,16);"`
+	From				  int64   `gorm:"column:create_time";type:bigint`
+	To				      int64   `gorm:"column:create_time";type:bigint`
 }
 
 func (s *RdsServiceImpl) create(trend Trend) error {
---
+++ market/trendmanager.go
@@ -0,0 +1,77 @@
+package market
+
+import "sync"
+
+type Ticker struct {
+	Market	              string
+	AmountS               float64
+	AmountB               float64
+	Open                  float64
+	Close                 float64
+	High                  float64
+	Low                   float64
+}
+
+type Trend struct {
+	Interval              string
+	Market	              string
+	AmountS               []byte
+	AmountB               []byte
+	CreateTime            int64
+	Open                  float64
+	Close                 float64
+	High                  float64
+	Low                   float64
+	From				  int64
+	To				      int64
+}
+
+type TrendManager struct {
+	TickerCache map[string]Ticker
+	TrendCache map[string][]Trend
+	IsTickerReady bool
+	IsTrendReady bool
+	EnableCache bool
+}
+
+var once sync.Once
+var trendManager TrendManager
+var tickerReadyChan chan bool
+var trendReadyChan chan bool
+
+func NewTrendManager(enableCache bool) TrendManager {
+	once.Do(func () {
+		trendManager = TrendManager{EnableCache:enableCache}
+		trendManager.TickerCache = make(map[string]Ticker)
+		trendManager.TrendCache = make(map[string][]Trend)
+
+		if enableCache {
+
+		}
+	})
+
+	return trendManager
+}
+
+var tickerCache map[string]Ticker
+
+// now every hour invoke this
+func GenerateTrend(interval string) error {
+
+	return nil
+}
+
+func GetTicker(market string) ([] Ticker, error) {
+
+	return nil, nil
+}
+
+// when order filled event comming, invoke this
+func UpdateTicker(market string, ticker Ticker) {
+	tickerCache[market] = ticker
+}
+
+func GetTrends(market string) ([]Trend, error) {
+
+	return nil, nil
+}
