ed27323c6e0c4a6115f824256c65b5b0437a555d
符坤 <fukun@fukundeMacBook-Pro.local>
2017-11-30 11:46:57 +0800

    ringmined event add splitS and splitB fields

--- dao/fill.go
+++ dao/fill.go
@@ -24,25 +24,26 @@ import (
 )
 
 type FillEvent struct {
-	ID                    int    `gorm:"column:id;primary_key;"`
-	Protocol              string `gorm:"column:contract_address;type:varchar(42)"`
-	Owner                 string `gorm:"column:owner;type:varchar(42)"`
-	RingIndex             int64  `gorm:"column:ring_index;"`
-	BlockNumber           int64  `gorm:"column:block_number"`
-	CreateTime            int64  `gorm:"column:create_time"`
-	RingHash              string `gorm:"column:ring_hash;varchar(82)"`
-	TxHash                string `gorm:"column:tx_hash;type:varchar(82)"`
-	PreOrderHash          string `gorm:"column:pre_order_hash;varchar(82)"`
-	NextOrderHash         string `gorm:"column:next_order_hash;varchar(82)"`
-	OrderHash             string `gorm:"column:order_hash;type:varchar(82)"`
-	AmountS               []byte `gorm:"column:amount_s;type:varchar(30)"`
-	AmountB               []byte `gorm:"column:amount_b;type:varchar(30)"`
-	TokenS                string `gorm:"column:token_s;type:varchar(42)"`
-	TokenB                string `gorm:"column:token_b;type:varchar(42)"`
-	LrcReward             []byte `gorm:"column:lrc_reward;type:varchar(30)"`
-	LrcFee                []byte `gorm:"column:lrc_fee;type:varchar(30)"`
-	MarginSplitPercentage int    `gorm:"column:margin_split_percentage"`
-	IsDeleted             bool   `gorm:"column:is_deleted"`
+	ID            int    `gorm:"column:id;primary_key;"`
+	Protocol      string `gorm:"column:contract_address;type:varchar(42)"`
+	Owner         string `gorm:"column:owner;type:varchar(42)"`
+	RingIndex     int64  `gorm:"column:ring_index;"`
+	BlockNumber   int64  `gorm:"column:block_number"`
+	CreateTime    int64  `gorm:"column:create_time"`
+	RingHash      string `gorm:"column:ring_hash;varchar(82)"`
+	TxHash        string `gorm:"column:tx_hash;type:varchar(82)"`
+	PreOrderHash  string `gorm:"column:pre_order_hash;varchar(82)"`
+	NextOrderHash string `gorm:"column:next_order_hash;varchar(82)"`
+	OrderHash     string `gorm:"column:order_hash;type:varchar(82)"`
+	AmountS       []byte `gorm:"column:amount_s;type:varchar(30)"`
+	AmountB       []byte `gorm:"column:amount_b;type:varchar(30)"`
+	TokenS        string `gorm:"column:token_s;type:varchar(42)"`
+	TokenB        string `gorm:"column:token_b;type:varchar(42)"`
+	LrcReward     []byte `gorm:"column:lrc_reward;type:varchar(30)"`
+	LrcFee        []byte `gorm:"column:lrc_fee;type:varchar(30)"`
+	SplitS        []byte `gorm:"column:split_s;type:varchar(30)"`
+	SplitB        []byte `gorm:"column:split_b;type:varchar(30)"`
+	IsDeleted     bool   `gorm:"column:is_deleted"`
 }
 
 // convert chainclient/orderFilledEvent to dao/fill
@@ -61,6 +62,12 @@ func (f *FillEvent) ConvertDown(src *types.OrderFilledEvent) error {
 	if f.LrcFee, err = src.LrcFee.MarshalText(); err != nil {
 		return err
 	}
+	if f.SplitS, err = src.SplitS.MarshalText(); err != nil {
+		return err
+	}
+	if f.SplitB, err = src.SplitB.MarshalText(); err != nil {
+		return err
+	}
 
 	f.Protocol = src.ContractAddress.Hex()
 	f.RingIndex = src.RingIndex.Int64()
@@ -74,7 +81,6 @@ func (f *FillEvent) ConvertDown(src *types.OrderFilledEvent) error {
 	f.TokenS = src.TokenS.Hex()
 	f.TokenB = src.TokenB.Hex()
 	f.Owner = src.Owner.Hex()
-	f.MarginSplitPercentage = src.MarginSplitPercentage
 	f.IsDeleted = src.IsDeleted
 
 	return nil
