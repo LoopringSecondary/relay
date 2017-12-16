6b3f0ed50dd12b3a088f08e677b519ebf40e5df3
符坤 <fukun@fukundeMacBook-Pro.local>
2017-12-16 16:51:50 +0800

    optimize accessor_test.go

--- ethaccessor/accessor_test.go
+++ ethaccessor/accessor_test.go
@@ -20,6 +20,7 @@ package ethaccessor_test
 
 import (
 	"github.com/Loopring/relay/dao"
+	"github.com/Loopring/relay/market/util"
 	"github.com/Loopring/relay/test"
 	"github.com/Loopring/relay/types"
 	"github.com/ethereum/go-ethereum/accounts"
@@ -28,17 +29,22 @@ import (
 	"testing"
 )
 
-const (
+var (
 	version              = test.Version
 	cancelOrderHash      = "0x296c419975d523f0427231c9aa0192bf9972b15731408c5e214227c95ddf991d"
-	cutOffOwner          = "0xb1018949b241D76A1AB2094f473E9bEfeAbB5Ead"
 	registerTokenAddress = "0x8b62ff4ddc9baeb73d0a3ea49d43e4fe8492935a"
 	registerTokenSymbol  = "wrdn"
-	wethOwner            = "0x1b978a1d302335a6f2ebe4b8823b5e17c3c84135"
-	transferToAccount    = "0xb1018949b241d76a1ab2094f473e9befeabb5ead"
-	lrcTokenAddress      = "0xEf82c1e65A207b0f1d0c311f48556F1C3979a822"
-	wethTokenAddress     = "0xfc2cbce778ddbc4d50bb5b2fc91afe14a8e3953d"
+	account1             = test.Entity().Accounts[0].Address
+	account2             = test.Entity().Accounts[1].Address
+	lrcTokenAddress      = util.AllTokens["LRC"].Protocol
+	wethTokenAddress     = util.AllTokens["WETH"].Protocol
 )
+
+func TestEthNodeAccessor_SetTokenBalance(t *testing.T) {
+	owner := account2
+	amount := new(big.Int).Mul(big.NewInt(1e18), big.NewInt(1000000))
+	test.SetTokenBalance("LRC", owner, amount)
+}
 
 func TestEthNodeAccessor_Erc20Balance(t *testing.T) {
 	accessor, err := test.GenerateAccessor()
@@ -46,21 +52,21 @@ func TestEthNodeAccessor_Erc20Balance(t *testing.T) {
 		t.Fatalf("generate accessor error:%s", err.Error())
 	}
 
-	tokenAddress := common.HexToAddress(wethTokenAddress)
-	owner := common.HexToAddress(wethOwner)
+	owner := account2
+	tokenAddress := lrcTokenAddress
 	balance, err := accessor.Erc20Balance(tokenAddress, owner, "latest")
 	if err != nil {
 		t.Fatalf("accessor get erc20 balance error:%s", err.Error())
 	}
 
-	t.Log(balance.String())
+	t.Log(new(big.Rat).SetFrac(balance, big.NewInt(1e18)).FloatString(2))
 }
 
 func TestEthNodeAccessor_Approval(t *testing.T) {
-	account := accounts.Account{Address: common.HexToAddress(transferToAccount)}
+	account := accounts.Account{Address: account2}
 
-	tokenAddress := common.HexToAddress(wethTokenAddress)
-	spender := common.HexToAddress(lrcTokenAddress)
+	tokenAddress := wethTokenAddress
+	spender := lrcTokenAddress
 	amount, _ := new(big.Int).SetString("2000000000000000000", 0)
 	accessor, _ := test.GenerateAccessor()
 	callMethod := accessor.ContractSendTransactionMethod(accessor.Erc20Abi, tokenAddress)
@@ -77,9 +83,9 @@ func TestEthNodeAccessor_Allowance(t *testing.T) {
 		t.Fatalf("generate accessor error:%s", err.Error())
 	}
 
-	owner := common.HexToAddress(transferToAccount)
-	tokenAddress := common.HexToAddress(wethTokenAddress)
-	spender := common.HexToAddress(lrcTokenAddress)
+	owner := account2
+	tokenAddress := wethTokenAddress
+	spender := lrcTokenAddress
 
 	if allowance, err := accessor.Erc20Allowance(tokenAddress, owner, spender, "latest"); err != nil {
 		t.Fatalf("accessor get erc20 approval error:%s", err.Error())
@@ -147,7 +153,7 @@ func TestEthNodeAccessor_GetCancelledOrFilled(t *testing.T) {
 // cutoff的值必须在两个块的timestamp之间
 func TestEthNodeAccessor_Cutoff(t *testing.T) {
 	c := test.Cfg()
-	account := accounts.Account{Address: common.HexToAddress(cutOffOwner)}
+	account := accounts.Account{Address: account1}
 	cutoff := big.NewInt(1522651087)
 
 	accessor, _ := test.GenerateAccessor()
@@ -164,8 +170,9 @@ func TestEthNodeAccessor_GetCutoff(t *testing.T) {
 	c := test.Cfg()
 	accessor, _ := test.GenerateAccessor()
 
+	owner := account1
 	protocol := common.HexToAddress(c.Common.ProtocolImpl.Address[version])
-	if timestamp, err := accessor.GetCutoff(protocol, common.HexToAddress(cutOffOwner), "latest"); err != nil {
+	if timestamp, err := accessor.GetCutoff(protocol, owner, "latest"); err != nil {
 		t.Fatal(err)
 	} else {
 		t.Logf("cutoff timestamp:%s", timestamp.String())
@@ -258,9 +265,9 @@ func TestEthNodeAccessor_IsAddressAuthorized(t *testing.T) {
 }
 
 func TestEthNodeAccessor_WethDeposit(t *testing.T) {
-	account := accounts.Account{Address: common.HexToAddress(wethOwner)}
+	account := accounts.Account{Address: account1}
 
-	wethAddr := common.HexToAddress(wethTokenAddress)
+	wethAddr := wethTokenAddress
 	amount := new(big.Int).SetInt64(1000000)
 	accessor, _ := test.GenerateAccessor()
 	callMethod := accessor.ContractSendTransactionMethod(accessor.WethAbi, wethAddr)
@@ -272,9 +279,9 @@ func TestEthNodeAccessor_WethDeposit(t *testing.T) {
 }
 
 func TestEthNodeAccessor_WethWithdrawal(t *testing.T) {
-	account := accounts.Account{Address: common.HexToAddress(wethOwner)}
+	account := accounts.Account{Address: account1}
 
-	wethAddr := common.HexToAddress(wethTokenAddress)
+	wethAddr := wethTokenAddress
 	amount, _ := new(big.Int).SetString("100", 0)
 	accessor, _ := test.GenerateAccessor()
 	callMethod := accessor.ContractSendTransactionMethod(accessor.WethAbi, wethAddr)
@@ -286,11 +293,11 @@ func TestEthNodeAccessor_WethWithdrawal(t *testing.T) {
 }
 
 func TestEthNodeAccessor_WethTransfer(t *testing.T) {
-	account := accounts.Account{Address: common.HexToAddress(wethOwner)}
+	account := accounts.Account{Address: account1}
 
-	wethAddr := common.HexToAddress(wethTokenAddress)
+	wethAddr := wethTokenAddress
 	amount := new(big.Int).SetInt64(100)
-	to := common.HexToAddress(transferToAccount)
+	to := account2
 	accessor, _ := test.GenerateAccessor()
 	callMethod := accessor.ContractSendTransactionMethod(accessor.WethAbi, wethAddr)
 	if result, err := callMethod(account, "transfer", big.NewInt(200000), big.NewInt(21000000000), nil, to, amount); nil != err {
