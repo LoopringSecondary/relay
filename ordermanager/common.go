fc5d5da0fe564c3f40e3d675f4fc04983371736c
符坤 <fukun@fukundeMacBook-Pro.local>
2017-12-18 15:01:10 +0800

    get price for order use correct function

--- ordermanager/common.go
+++ ordermanager/common.go
@@ -81,19 +81,18 @@ func isOrderFullFinished(state *types.OrderState, mc marketcap.MarketCapProvider
 		cancelOrFilledAmountB := new(big.Int).Add(dealtAndSplitAmountB, state.CancelledAmountB)
 		remainAmountB := new(big.Int).Sub(state.RawOrder.AmountB, cancelOrFilledAmountB)
 		ratRemainAmountB := new(big.Rat).SetInt(remainAmountB)
-		price, _ := mc.GetMarketCap(state.RawOrder.TokenB)
-		valueOfRemainAmount = new(big.Rat).Mul(price, ratRemainAmountB)
+		valueOfRemainAmount, _ = mc.LegalCurrencyValue(state.RawOrder.TokenB, ratRemainAmountB)
 	} else {
 		dealtAndSplitAmountS := new(big.Int).Add(state.DealtAmountS, state.SplitAmountS)
 		cancelOrFilledAmountS := new(big.Int).Add(dealtAndSplitAmountS, state.CancelledAmountS)
 		remainAmountS := new(big.Int).Sub(state.RawOrder.AmountS, cancelOrFilledAmountS)
 		ratRemainAmountS := new(big.Rat).SetInt(remainAmountS)
-		price, _ := mc.GetMarketCap(state.RawOrder.TokenS)
-		valueOfRemainAmount = new(big.Rat).Mul(price, ratRemainAmountS)
+		valueOfRemainAmount, _ = mc.LegalCurrencyValue(state.RawOrder.TokenS, ratRemainAmountS)
 	}
 
-	// todo: get compare number from config
-	if valueOfRemainAmount.Cmp(big.NewRat(1, 1)) > 0 {
+	// todo: get compare number from config, and if valueOfRemainAmount is nil procedure of this may have problem
+	minValue := big.NewInt(1)
+	if valueOfRemainAmount == nil || valueOfRemainAmount.Cmp(new(big.Rat).SetInt(minValue)) > 0 {
 		return false
 	}
 
