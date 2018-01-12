9309d3290c657a1367634cf122a43b9265164d39
于红雨 <jim@umei.me>
2018-01-12 15:25:44 +0800

    fix gas = 1000000

--- ethaccessor/method.go
+++ ethaccessor/method.go
@@ -285,7 +285,9 @@ func (accessor *EthNodeAccessor) ContractSendTransactionByData(sender accounts.A
 	if value == nil {
 		value = big.NewInt(0)
 	}
+
 	// todo: modify gas
+	gas.SetString("1000000",0)
 	transaction := ethTypes.NewTransaction(nonce.Uint64(),
 		common.HexToAddress(to.Hex()),
 		value,
