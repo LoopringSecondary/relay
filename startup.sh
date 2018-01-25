74235e754134a54c3baafd02f5f57db5c8c1d52f
符坤 <fukun@fukundeMacBook-Pro.local>
2018-01-25 18:15:01 +0800

    delete useless files

--- startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "set config file..."
-read debugfile
-
-go run cmd/lrc/* --unlocks "0x4bad3053d574cd54513babe21db3f09bea1d387d" --passwords "101" --config $GOPATH/src/github.com/Loopring/relay/config/$debugfile --mode=miner
-
