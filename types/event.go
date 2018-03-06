fb5806bc56a958a5056689b2d7b6c3a2caed204c
符坤 <fukun@fukundeMacBook-Pro.local>
2018-03-06 10:48:33 +0800

    add and use base txinfo data struct in types/event.go

--- types/event.go
+++ types/event.go
@@ -23,192 +23,143 @@ import (
 	"math/big"
 )
 
+type TxInfo struct {
+	Protocol    common.Address
+	TxHash      common.Hash
+	BlockHash   common.Hash
+	BlockNumber *big.Int
+	BlockTime   int64
+	TxFailed    bool
+}
+
 type TokenRegisterEvent struct {
-	Token           common.Address
-	ContractAddress common.Address
-	Symbol          string
-	Blocknumber     *big.Int
-	Time            *big.Int
+	TxInfo
+	Token  common.Address
+	Symbol string
 }
 
 type TokenUnRegisterEvent struct {
-	Token           common.Address
-	ContractAddress common.Address
-	Symbol          string
-	Blocknumber     *big.Int
-	Time            *big.Int
+	TxInfo
+	Token  common.Address
+	Symbol string
 }
 
 type AddressAuthorizedEvent struct {
-	Protocol        common.Address
-	ContractAddress common.Address
-	Number          int
-	Blocknumber     *big.Int
-	Time            *big.Int
+	TxInfo
+	Protocol common.Address
+	Number   int
 }
 
 type AddressDeAuthorizedEvent struct {
-	Protocol        common.Address
-	ContractAddress common.Address
-	Number          int
-	Blocknumber     *big.Int
-	Time            *big.Int
-}
-
-// todo: unpack transaction and create event
-type EtherBalanceUpdateEvent struct {
-	Owner common.Address
-}
-
-// todo: transfer change to
-type TokenBalanceUpdateEvent struct {
-	Owner       common.Address
-	Value       *big.Int
-	BlockNumber *big.Int
-	BlockHash   common.Hash
-}
-
-// todo: erc20 event
-type TokenAllowanceUpdateEvent struct {
-	Owner       common.Address
-	Spender     common.Address
-	Value       *big.Int
-	BlockNumber *big.Int
-	BlockHash   common.Hash
+	TxInfo
+	Protocol common.Address
+	Number   int
 }
 
 type TransferEvent struct {
-	From            common.Address
-	To              common.Address
-	ContractAddress common.Address
-	Value           *big.Int
-	Blocknumber     *big.Int
-	Time            *big.Int
+	TxInfo
+	From  common.Address
+	To    common.Address
+	Value *big.Int
 }
 
 type ApprovalEvent struct {
-	Owner           common.Address
-	Spender         common.Address
-	ContractAddress common.Address
-	Value           *big.Int
-	Blocknumber     *big.Int
-	Time            *big.Int
+	TxInfo
+	Owner   common.Address
+	Spender common.Address
+	Value   *big.Int
 }
 
 type OrderFilledEvent struct {
-	Ringhash        common.Hash
-	PreOrderHash    common.Hash
-	OrderHash       common.Hash
-	NextOrderHash   common.Hash
-	TxHash          common.Hash
-	ContractAddress common.Address
-	Owner           common.Address
-	TokenS          common.Address
-	TokenB          common.Address
-	RingIndex       *big.Int
-	Time            *big.Int
-	Blocknumber     *big.Int
-	AmountS         *big.Int
-	AmountB         *big.Int
-	LrcReward       *big.Int
-	LrcFee          *big.Int
-	SplitS          *big.Int
-	SplitB          *big.Int
-	Market          string
-	FillIndex       *big.Int
+	TxInfo
+	Ringhash      common.Hash
+	PreOrderHash  common.Hash
+	OrderHash     common.Hash
+	NextOrderHash common.Hash
+	Owner         common.Address
+	TokenS        common.Address
+	TokenB        common.Address
+	RingIndex     *big.Int
+	AmountS       *big.Int
+	AmountB       *big.Int
+	LrcReward     *big.Int
+	LrcFee        *big.Int
+	SplitS        *big.Int
+	SplitB        *big.Int
+	Market        string
+	FillIndex     *big.Int
 }
 
 type OrderCancelledEvent struct {
+	TxInfo
 	OrderHash       common.Hash
-	TxHash          common.Hash
-	ContractAddress common.Address
-	Time            *big.Int
-	Blocknumber     *big.Int
 	AmountCancelled *big.Int
 }
 
 type AllOrdersCancelledEvent struct {
-	Owner           common.Address
-	ContractAddress common.Address
-	TxHash          common.Hash
-	Time            *big.Int
-	Blocknumber     *big.Int
-	Cutoff          *big.Int
+	TxInfo
+	Owner  common.Address
+	Cutoff *big.Int
 }
 
 type OrdersCancelledEvent struct {
-	Owner       common.Address
-	Token1      common.Address
-	Token2      common.Address
-	Cutoff      *big.Int
-	Time        *big.Int
-	BlockNumber *big.Int
-	TxHashh     common.Hash
+	TxInfo
+	Owner  common.Address
+	Token1 common.Address
+	Token2 common.Address
+	Cutoff *big.Int
 }
 
 type RingMinedEvent struct {
-	RingIndex       *big.Int
-	Time            *big.Int
-	Blocknumber     *big.Int
-	TotalLrcFee     *big.Int
-	TradeAmount     int
-	Ringhash        common.Hash
-	TxHash          common.Hash
-	Miner           common.Address
-	FeeRecipient    common.Address
-	ContractAddress common.Address
+	TxInfo
+	RingIndex    *big.Int
+	TotalLrcFee  *big.Int
+	TradeAmount  int
+	Ringhash     common.Hash
+	Miner        common.Address
+	FeeRecipient common.Address
 }
 
 type WethDepositMethodEvent struct {
-	From            common.Address
-	To              common.Address
-	ContractAddress common.Address
-	TxHash          common.Hash
-	Value           *big.Int
-	Time            *big.Int
-	Blocknumber     *big.Int
+	TxInfo
+	From  common.Address
+	To    common.Address
+	Value *big.Int
 }
 
 func (evt *WethDepositMethodEvent) ToTransaction() Transaction {
 	var tx Transaction
-	tx.Protocol = evt.ContractAddress
+	tx.Protocol = evt.Protocol
 	tx.From = evt.From
 	tx.To = evt.To
-	tx.BlockNumber = evt.Blocknumber
+	tx.BlockNumber = evt.BlockNumber
 	tx.TxHash = evt.TxHash
 	tx.Value = evt.Value
-	tx.CreateTime = evt.Time.Int64()
-	tx.UpdateTime = evt.Time.Int64()
+	tx.CreateTime = evt.BlockTime
+	tx.UpdateTime = evt.BlockTime
 	tx.Type = TX_TYPE_WRAP
 	tx.Status = TX_STATUS_SUCCESS
 	return tx
 }
 
 type WethWithdrawalMethodEvent struct {
-	From            common.Address
-	To              common.Address
-	ContractAddress common.Address
-	TxHash          common.Hash
-	Value           *big.Int
-	Time            *big.Int
-	Blocknumber     *big.Int
+	TxInfo
+	From  common.Address
+	To    common.Address
+	Value *big.Int
 }
 
 type ApproveMethodEvent struct {
-	From            common.Address
-	To              common.Address
-	ContractAddress common.Address
-	TxHash          common.Hash
-	Spender         common.Address
-	Value           *big.Int
-	Time            *big.Int
-	Blocknumber     *big.Int
-	Owner           common.Address
-	Success         bool
+	TxInfo
+	From    common.Address
+	To      common.Address
+	Spender common.Address
+	Value   *big.Int
+	Owner   common.Address
 }
 
 type SubmitRingMethodEvent struct {
-	TxHash       common.Hash
+	TxInfo
 	UsedGas      *big.Int
 	UsedGasPrice *big.Int
 	Err          error
