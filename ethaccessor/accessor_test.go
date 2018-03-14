5f1a480704d643bddc018c047933e6911a507e0d
符坤 <fukun@fukundeMacBook-Pro.local>
2018-03-14 13:25:42 +0800

    test weth deposit/withdrawal and approve

--- ethaccessor/accessor_test.go
+++ ethaccessor/accessor_test.go
@@ -35,6 +35,7 @@ var (
 	version              = test.Version
 	registerTokenAddress = "0x8b62ff4ddc9baeb73d0a3ea49d43e4fe8492935a"
 	registerTokenSymbol  = "wrdn"
+	miner                = test.Entity().Creator
 	account1             = test.Entity().Accounts[0].Address
 	account2             = test.Entity().Accounts[1].Address
 	lrcTokenAddress      = util.AllTokens["LRC"].Protocol
@@ -271,10 +272,9 @@ func TestEthNodeAccessor_IsAddressAuthorized(t *testing.T) {
 }
 
 func TestEthNodeAccessor_WethDeposit(t *testing.T) {
-	account := accounts.Account{Address: account1}
-
+	account := miner
 	wethAddr := wethTokenAddress
-	amount, _ := new(big.Int).SetString("100000000000000000000000000000000000000000000000000000000000000", 0)
+	amount := new(big.Int).Mul(big.NewInt(1e18), big.NewInt(1))
 	callMethod := ethaccessor.ContractSendTransactionMethod("latest", ethaccessor.WethAbi(), wethAddr)
 	if result, err := callMethod(account.Address, "deposit", big.NewInt(200000), big.NewInt(21000000000), amount); nil != err {
 		t.Fatalf("call method weth-deposit error:%s", err.Error())
@@ -284,10 +284,9 @@ func TestEthNodeAccessor_WethDeposit(t *testing.T) {
 }
 
 func TestEthNodeAccessor_WethWithdrawal(t *testing.T) {
-	account := accounts.Account{Address: account1}
-
+	account := miner
 	wethAddr := wethTokenAddress
-	amount, _ := new(big.Int).SetString("100", 0)
+	amount := new(big.Int).Mul(big.NewInt(1e18), big.NewInt(1))
 	callMethod := ethaccessor.ContractSendTransactionMethod("latest", ethaccessor.WethAbi(), wethAddr)
 	if result, err := callMethod(account.Address, "withdraw", big.NewInt(200000), big.NewInt(21000000000), nil, amount); nil != err {
 		t.Fatalf("call method weth-withdraw error:%s", err.Error())
