2a7329eeda2e519f21b9d37ebba316b25ba95a3e
符坤 <fukun@fukundeMacBook-Pro.local>
2018-03-25 19:00:45 +0800

    modify big number in mysql tables

--- dao/ringmined.go
+++ dao/ringmined.go
@@ -28,14 +28,14 @@ import (
 type RingMinedEvent struct {
 	ID                 int    `gorm:"column:id;primary_key" json:"id"`
 	Protocol           string `gorm:"column:contract_address;type:varchar(42)" json:"protocol"`
-	RingIndex          string `gorm:"column:ring_index;type:varchar(30);unique_index" json:"ringIndex"`
+	RingIndex          string `gorm:"column:ring_index;type:varchar(40);unique_index" json:"ringIndex"`
 	RingHash           string `gorm:"column:ring_hash;type:varchar(82)" json:"ringHash"`
 	TxHash             string `gorm:"column:tx_hash;type:varchar(82)" json:"txHash"`
 	Miner              string `gorm:"column:miner;type:varchar(42);" json:"miner"`
 	FeeRecipient       string `gorm:"column:fee_recipient;type:varchar(42)" json:"feeRecipient"`
 	IsRinghashReserved bool   `gorm:"column:is_ring_hash_reserved;" json:"isRinghashReserved"`
 	BlockNumber        int64  `gorm:"column:block_number;type:bigint" json:"blockNumber"`
-	TotalLrcFee        string `gorm:"column:total_lrc_fee;type:varchar(30)" json:"totalLrcFee"`
+	TotalLrcFee        string `gorm:"column:total_lrc_fee;type:varchar(40)" json:"totalLrcFee"`
 	TradeAmount        int    `gorm:"column:trade_amount" json:"tradeAmount"`
 	Time               int64  `gorm:"column:time;type:bigint" json:"timestamp"`
 }
