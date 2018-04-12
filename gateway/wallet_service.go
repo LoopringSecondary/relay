7cc80333ac2acf334b7ef6a1816028353cfe5290
mmmmmagina <jaice_229@163.com>
2018-04-12 19:12:40 +0800

    add mock data for foo and bar

--- gateway/wallet_service.go
+++ gateway/wallet_service.go
@@ -376,6 +376,12 @@ func (w *WalletServiceImpl) GetPriceQuote(query PriceQuoteQuery) (result PriceQu
 		}
 	}
 
+	// mock data . will delete later
+	fooPrice := TokenPrice{"FOO", rst.Tokens[1].Price}
+	barPrice := TokenPrice{"BAR", rst.Tokens[2].Price}
+	rst.Tokens = append(rst.Tokens, fooPrice)
+	rst.Tokens = append(rst.Tokens, barPrice)
+
 	return rst, nil
 }
 
