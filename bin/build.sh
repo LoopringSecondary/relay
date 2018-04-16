f7f8c2442951c6c910ca7fc126b880d0874ef6d5
liweichao <weichao092@163.com>
2018-04-16 20:08:56 +0800

    fix bug for config

--- bin/build.sh
+++ bin/build.sh
@@ -4,11 +4,11 @@
 WORK_DIR=/opt/loopring/relay
 SVC_DIR=/etc/service/relay
 GOROOT=/usr/lib/go-1.9
-PATH=$PATH:$GOROOT/bin
-GOPATH=/opt/loopring/go-src
+export PATH=$PATH:$GOROOT/bin
+export GOPATH=/opt/loopring/go-src
 
 #cp svc config to svc
-sudo cp -rf $WORK_DIR/bin/svc/* $SVC_DIR
+sudo cp -rf $WORK_DIR/src/bin/svc/* $SVC_DIR
 sudo chmod -R 755 $SVC_DIR
 
 SRC_DIR=$GOPATH/github.com/Loopring/relay
@@ -20,5 +20,5 @@ cd $SRC_DIR
 sudo rm -rf ./*
 sudo cp -r $WORK_DIR/src/* ./
 sudo chmod -R 333 ./
-make relay
+go build -ldflags -s -v  -o build/bin/relay cmd/lrc/*
 cp build/bin/relay $WORK_DIR/bin
