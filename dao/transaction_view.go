ba2b6201be5bd288a5f7f3a895e7247441874589
符坤 <fukun@fukundeMacBook-Pro.local>
2018-04-27 19:37:05 +0800

    fix silly bug

--- dao/transaction_view.go
+++ dao/transaction_view.go
@@ -80,10 +80,11 @@ func (tx *TransactionView) ConvertUp(dst *txtyp.TransactionView) error {
 //////////// write related
 
 // 根据owner&hash查询pending tx
-func (s *RdsServiceImpl) FindPendingTxViewByOwnerAndHash(owner, hash string) ([]TransactionView, error) {
+func (s *RdsServiceImpl) FindPendingTxViewByOwnerAndHash(symbol, owner, hash string) ([]TransactionView, error) {
 	var txs []TransactionView
 
 	err := s.db.Where("owner=?", owner).
+		Where("symbol=?", symbol).
 		Where("tx_hash=?", hash).
 		Where("status=?", types.TX_STATUS_PENDING).
 		Where("fork=?", false).
