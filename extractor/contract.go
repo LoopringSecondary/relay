aa46b77bd90d73d33581e5091f060daf9f75abca
符坤 <fukun@fukundeMacBook-Pro.local>
2018-05-08 13:24:20 +0800

    comment emit order

--- extractor/contract.go
+++ extractor/contract.go
@@ -483,11 +483,11 @@ func (processor *AbiProcessor) handleSubmitRingMethod(input eventemitter.EventDa
 	}
 
 	// 不需要发送订单到gateway
-	for _, v := range event.OrderList {
-		v.Hash = v.GenerateHash()
-		log.Debugf("extractor,tx:%s submitRing method orderHash:%s,owner:%s,tokenS:%s,tokenB:%s,amountS:%s,amountB:%s", event.TxHash.Hex(), v.Hash.Hex(), v.Owner.Hex(), v.TokenS.Hex(), v.TokenB.Hex(), v.AmountS.String(), v.AmountB.String())
-		//eventemitter.Emit(eventemitter.GatewayNewOrder, v)
-	}
+	//for _, v := range event.OrderList {
+	//	v.Hash = v.GenerateHash()
+	//	log.Debugf("extractor,tx:%s submitRing method orderHash:%s,owner:%s,tokenS:%s,tokenB:%s,amountS:%s,amountB:%s", event.TxHash.Hex(), v.Hash.Hex(), v.Owner.Hex(), v.TokenS.Hex(), v.TokenB.Hex(), v.AmountS.String(), v.AmountB.String())
+	//	eventemitter.Emit(eventemitter.GatewayNewOrder, v)
+	//}
 
 	log.Debugf("extractor,tx:%s submitRing method gas:%s, gasprice:%s, status:%s", event.TxHash.Hex(), event.GasUsed.String(), event.GasPrice.String(), types.StatusStr(event.Status))
 
