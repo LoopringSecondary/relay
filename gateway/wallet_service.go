11c0ee99318668ee4a3414f1e41ddafbaff5073b
mmmmmagina <jaice_229@163.com>
2018-05-17 18:49:08 +0800

    extend depth

--- gateway/wallet_service.go
+++ gateway/wallet_service.go
@@ -549,7 +549,7 @@ func (w *WalletServiceImpl) SubmitRingForP2P(p2pRing P2PRingRequest) (res string
 
 func (w *WalletServiceImpl) GetDepth(query DepthQuery) (res Depth, err error) {
 
-	defaultDepthLength := 20
+	defaultDepthLength := 50
 
 	mkt := strings.ToUpper(query.Market)
 	delegateAddress := query.DelegateAddress
